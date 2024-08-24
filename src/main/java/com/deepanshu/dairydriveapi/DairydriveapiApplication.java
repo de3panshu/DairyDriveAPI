a7a2b4c86af61915c8ea0abf1235a8a1246b039f
Deepanshu <sahu.deeepanshu.united@gmail.com>
2024-08-24 13:45:39 +0530

    Initial Setup done

---
+++ src/main/java/com/deepanshu/dairydriveapi/DairydriveapiApplication.java
@@ -0,0 +1,13 @@
+package com.deepanshu.dairydriveapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DairydriveapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DairydriveapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deepanshu/dairydriveapi/DairydriveapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepanshu.dairydriveapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DairydriveapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
